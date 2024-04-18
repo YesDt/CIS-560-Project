8070722db05883135e2d1867507c578535f6ff46
Darius Tyson <dariust@ksu.edu>
2024-04-18 03:32:40 -0500

    Adding games page

---
+++ 560Project/Pages/Games.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _560Project.Pages
+{
+    public class GamesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
