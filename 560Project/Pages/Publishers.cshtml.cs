eef38c92dd83c17b3dd34d7aca5275c7bb2eda66
Darius Tyson <dariust@ksu.edu>
2024-04-18 03:41:01 -0500

    Added pages for everything else

---
+++ 560Project/Pages/Publishers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _560Project.Pages
+{
+    public class PublishersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
