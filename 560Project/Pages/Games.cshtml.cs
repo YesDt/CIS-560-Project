9364cd9c830b6d845b44123ee507c50d5a354898
Darius Tyson <dariust@ksu.edu>
2024-04-19 03:06:38 -0500

    Figured out how to connect. Now we just gotta find out how to display the tables

--- 560Project/Pages/Games.cshtml.cs
+++ 560Project/Pages/Games.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace _560Project.Pages
 {
+    ///private readonly GameContext _context;
+
     public class GamesModel : PageModel
     {
         public void OnGet()
